1d80b52afde27b86ebff067de5388d63ce78e146
gtbhv8q8tk-design <gtbhv8q8tk@privaterelay.appleid.com>
2025-10-30 15:16:07 +0200

    Refactor NFT minting response structure
    Updated the NFT minting handler to include new fields and change the response structure.

--- api/mint.js
+++ api/mint.js
@@ -1,21 +1,15 @@
-export default function handler(req, res) {
-  res.status(402).json({
-    x402Version: 1,
-    resources: [
-      {
-        id: "1",
-        title: "Mint Frog NFT",
-        description: "Pay 1 USDC to mint a Frog NFT",
-        media: "https://ipfs.io/ipfs/QmepBFK4YT8KwB4GNg3pwBdtDJy8kr8RtPgURTBdqt8fV8/1.png",
-        accepts: [
-          {
-            chain: "base",
-            currency: "USDC",
-            amount: "1000000", // 1 USDC = 1_000_000 (6 decimals)
-            receiver: "0x63AcAd363c60178e0153268a272876197770bFEf"
-          }
-        ]
-      }
-    ]
-  });
+{
+  "x402Version": 1,
+  "name": "x402Frogs NFT Mint",
+  "description": "Pay 1 USDC to mint your frog NFT.",
+  "icon": "https://ipfs.io/ipfs/QmepBFK4YT8KwB4GNg3pwBdtDJy8kr8RtPgURTBdqt8fV8/1.png",
+  "accepts": [
+    {
+      "chain": "base",
+      "token": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", 
+      "amount": "1000000",
+      "to": "0xТвоя_адреса",
+      "label": "Mint NFT"
+    }
+  ]
 }
